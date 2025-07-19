014f04e6dc918742da49932f42901064155b7f13
Jaylan <jaylansaad20@gmail.com>
2025-07-19 17:25:17 +0300

    updated

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -3,28 +3,27 @@ import Square from "./Square";
 import './GameBoard.css';
 
 
-const GameBoard = ({ rows = 8, cols = 8 }) => {
-  const createBoard = () => {
-    const board = [];
-    for (let i = 0; i < rows * cols; i++) {
-      board.push(
+const GameBoard = ({
+  rows = 8,
+  cols = 8,
+  onSquareClick = () => {},
+  onSquareRightClick = () => {},
+  squareStates = [],
+}) => {
+  const totalSquares = rows * cols;
+
+  return (
+    <div className="game-board">
+      {Array.from({ length: totalSquares }, (_, i) => (
         <Square
           key={i}
-          onLeftClick={() => console.log(`Left click on ${i}`)}
-          onRightClick={() => console.log(`Right click on ${i}`)}
-          state="default"
+          onLeftClick={() => onSquareClick(i)}
+          onRightClick={() => onSquareRightClick(i)}
+          state={squareStates[i] || "default"}
         />
-      );
-    }
-
-    return board;
-
-  };
-
-  return <div className="game-board">{createBoard()}</div>;
+      ))}
+    </div>
+  );
 };
 
-
 export default GameBoard;
-
-
